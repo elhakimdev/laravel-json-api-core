62f6e724abacc373f76ad89b660e125975f7f83c
ElhakimDev <abdulelhakim68@gmail.com>
2022-02-08 17:53:24 +0700

    update docs

--- src/Modules/Server/Contracts/ServerContract.php
+++ src/Modules/Server/Contracts/ServerContract.php
@@ -18,11 +18,54 @@ interface ServerContract {
      * @return string The name of server.
      */
     public function getName();
+
+    /**
+     * Enabling server.
+     *
+     * @return Server
+     */
     public function enabled(): Server;
+
+    /**
+     * Disabling the server.
+     *
+     * @return Server
+     */
     public function disabled(): Server;
+
+    /**
+     * Activate the server.
+     *
+     * @return Server
+     */
     public function activated(): Server;
+
+    /**
+     * Deactivate the server.
+     *
+     * @return Server
+     */
     public function deactivated(): Server;
+
+    /**
+     * Check th active status of give server.
+     *
+     * @return boolean
+     */
     public function checkActiveStatus(): bool;
+
+    /**
+     * Check the availaable status of server.
+     *
+     * @return boolean
+     */
     public function checkEnableStatus(): bool;
+
+    /**
+     * Configure server.
+     *
+     * @param callable $configuration
+     * @return Server
+     */
     public function configure(callable $configuration): Server;
 }
